53ab93f2ada092b5d8d401475a00bfa794695fcd
Nancy <nancygoswami2000@gmail.com>
2022-12-06 16:25:16 +0530

    second

---
+++ build/precache-manifest.0c4261342b70287ab035574abca33007.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "36d840aa6acf16223afc",
+    "url": "/University_Hub/static/css/main.a42abdca.chunk.css"
+  },
+  {
+    "revision": "36d840aa6acf16223afc",
+    "url": "/University_Hub/static/js/main.61869c50.chunk.js"
+  },
+  {
+    "revision": "72e4086240391d42690a",
+    "url": "/University_Hub/static/js/runtime~main.24c6ac70.js"
+  },
+  {
+    "revision": "666b06f6fbfa138eb83a",
+    "url": "/University_Hub/static/js/2.1075eb96.chunk.js"
+  },
+  {
+    "revision": "2f5372180e6248b85609",
+    "url": "/University_Hub/static/js/3.9bbe71da.chunk.js"
+  },
+  {
+    "revision": "8e006c6dde735c7fdcf279b4af17f7c1",
+    "url": "/University_Hub/static/media/logo.8e006c6d.png"
+  },
+  {
+    "revision": "8af61c86df7bcaa4030fba4201d75913",
+    "url": "/University_Hub/static/media/thinkingman.8af61c86.webp"
+  },
+  {
+    "revision": "f022b4ba3344ac8a08babe929e1af2a5",
+    "url": "/University_Hub/static/media/college.f022b4ba.jpg"
+  },
+  {
+    "revision": "b130861415fad90cf9a85e3b05a1ab8e",
+    "url": "/University_Hub/index.html"
+  }
+];
